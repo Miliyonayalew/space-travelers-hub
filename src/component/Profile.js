b55e04935f1d30643841f0ead1e1229839ba3566
Miliyonayalew <miliayalew@gmail.com>
2022-11-15 18:44:05 +0300

    Install packages, Add components and navbar

---
+++ src/component/Profile.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Profile = () => (<div><h1>Mission</h1></div>);
+
+export default Profile;
---
+++ src/component/Rocket.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Rocket = () => (<div><h1>Rocket</h1></div>);
+
+export default Rocket;
