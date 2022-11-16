fd4d8c2383a9983408de65c54581e3b65c0c04de
Miliyonayalew <miliayalew@gmail.com>
2022-11-16 21:49:12 +0300

    Add switch badge for rocket badges componet and dispatch

--- src/component/Rocket.js
+++ src/component/Rocket.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { reserveRocket, cancelRocket } from '../redux/rocket/rocket';
 
-const Rocket = ({ rocket }) => (
-  <li key={rocket.id} className="rocket">
-    <img src={rocket.image} alt={rocket.name} />
-    <div className="rocket-info">
-      <h3 className="rocket-name">{rocket.name}</h3>
-      <div className="rocket-description">
-        <p>{rocket.description}</p>
+const Rocket = ({ rocket }) => {
+  const dispatch = useDispatch();
+
+  const handleReservation = () => {
+    if (rocket.reserved) {
+      dispatch(cancelRocket(rocket.id));
+    } else {
+      dispatch(reserveRocket(rocket.id));
+    }
+  };
+
+  return (
+    <li key={rocket.id} className="rocket">
+      <img src={rocket.image} alt={rocket.name} />
+      <div className="rocket-info">
+        <h3 className="rocket-name">{rocket.name}</h3>
+        <div className="rocket-description d-flex">
+          {rocket.reserved ? (
+            <h2 className={rocket.reserved ? 'reserved' : ''}>
+              Reserved
+            </h2>
+          ) : (
+            ''
+          )}
+          &nbsp;
+          {rocket.description}
+        </div>
+        <button
+          type="button"
+          onClick={handleReservation}
+          className={rocket.reserved ? 'cancel' : 'reserve'}
+        >
+          {rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+        </button>
       </div>
-      <button type="button">Reserve Rocket</button>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 Rocket.propTypes = {
   rocket: PropTypes.shape({
@@ -20,6 +48,7 @@ Rocket.propTypes = {
     name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
+    reserved: PropTypes.bool.isRequired,
   }).isRequired,
 };
 
