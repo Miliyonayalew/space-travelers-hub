9e2587162baa4cabfe62e9ebf0e426eef335b496
Miliyonayalew <miliayalew@gmail.com>
2022-11-16 15:54:48 +0300

    Display rocket list render by using useSelector

---
+++ src/component/Rockets.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Rocket from './Rocket';
+
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rocketsReducer.rockets);
+  return (
+    <ul>
+      {rockets?.map((rocket) => (
+        <Rocket key={rocket.id} rocket={rocket} />
+      ))}
+    </ul>
+  );
+};
+
+export default Rockets;
