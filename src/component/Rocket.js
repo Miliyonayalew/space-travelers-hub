d029433bc9f4b7e1c7a716533739d936ef38c19f
Miliyonayalew <miliayalew@gmail.com>
2022-11-16 15:54:14 +0300

    Display rocket list render

--- src/component/Rocket.js
+++ src/component/Rocket.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Rocket = () => (<div><h1>Rocket</h1></div>);
+const Rocket = ({ rocket }) => (
+  <li key={rocket.id} className="rocket">
+    <img src={rocket.image} alt={rocket.name} />
+    <div className="rocket-info">
+      <h3 className="rocket-name">{rocket.name}</h3>
+      <div className="rocket-description">
+        <p>{rocket.description}</p>
+      </div>
+      <button type="button">Reserve Rocket</button>
+    </div>
+  </li>
+);
+
+Rocket.propTypes = {
+  rocket: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Rocket;
