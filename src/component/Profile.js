facd9030ddfbe5edc2089977fa1e1e228f28306a
Miliyonayalew <miliayalew@gmail.com>
2022-11-17 17:48:44 +0300

    Add logo asset

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Profile = () => (<div><h1>Profile</h1></div>);
+const Profile = () => {
+  const reservedRockets = useSelector((state) => state.rocketsReducer.rockets);
+  const reservedMissions = useSelector((state) => state.missionsReducer.missions);
+
+  return (
+    <div className="container d-grid">
+      <div>
+        <h1 className="profile-title-mission">My Missions</h1>
+        <ul className="reserved-list-mission d-flex">
+          {reservedMissions
+            ?.filter((mission) => mission.status)
+            .map((mission) => (
+              <li key={mission.mission_id} className="reserved-item d-flex">
+                {mission.mission_name}
+              </li>
+            ))}
+        </ul>
+      </div>
+      <div>
+        <h1 className="profile-title-rocket">My Rockets</h1>
+        <ul className="reserved-list-rocket d-flex">
+          {reservedRockets
+            ?.filter((rocket) => rocket.reserved)
+            .map((rocket) => (
+              <li key={rocket.id} className="reserved-item d-flex">
+                {rocket.name}
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -3,15 +3,18 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rocketsReducer, { fetchRocketsThunk } from './rocket/rocket';
 import missionsReducer, { fetchMissionsThunk } from './mission/mission';
+import reservedRocketsReducer, { fetchReservedRocketsThunk } from './profile/profile';
 
 const store = configureStore({
   reducer: {
     rocketsReducer,
     missionsReducer,
+    reservedRocketsReducer,
   },
 }, applyMiddleware(thunk, logger));
 
 store.dispatch(fetchRocketsThunk());
 store.dispatch(fetchMissionsThunk());
+store.dispatch(fetchReservedRocketsThunk());
 
 export default store;
