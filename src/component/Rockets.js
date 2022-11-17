1554fe54ca1f1cc5439235418eb4ac347f8cae62
stanley <tochukwuu14@gmail.com>
2022-11-17 20:01:31 +0100

    make some changes

--- src/component/Rockets.js
+++ src/component/Rockets.js
@@ -5,7 +5,7 @@ import Rocket from './Rocket';
 const Rockets = () => {
   const rockets = useSelector((state) => state.rocketsReducer.rockets);
   return (
-    <ul>
+    <ul data-testid="rocket">
       {rockets?.map((rocket) => (
         <Rocket key={rocket.id} rocket={rocket} />
       ))}
